38d28bf6f8ab5af20c9c5a4aca6958f48d35f42d
MattTMiles <mtmil3@student.monash.edu>
2020-03-06 15:30:11 +1100

    fixed the done file writing so now it works

--- update.py
+++ update.py
@@ -22,8 +22,6 @@ for pulsar in targets:
     print({pulsar})
     #Make a directory for the data under MATTIME
     mkdirpath1 = os.path.join(chdirpath1, pulsar)
-    #This is just to get rid of the problem where it detects that the directory already exists
-    
     try:
         os.mkdir(mkdirpath1)
     except OSError as exc:
@@ -36,11 +34,16 @@ for pulsar in targets:
     #Move into the MEERTIME timing directory
     os.chdir(timing)
     print('In this directory',os.getcwd())
+    #In MEERTIME timing directory
     pulsarpath = os.path.join(timing, pulsar)
 
     #Move into the pulsar directory with command below
     os.chdir(pulsarpath)
+    #In individual pulsar directory
     print('In this directory', os.getcwd())
+    #observationpath = os.chdir(chdirpath3)
+
+    ##input here a way to set the beamno? Required?##
 
     #Create a for loop for the observations and roll through these
     for obs in os.listdir(pulsarpath):
@@ -48,35 +51,46 @@ for pulsar in targets:
         #Move into the direcotry for the individual observation
         os.chdir(observationpath)
         print('In this directory', os.getcwd())
+        #In individual pulsar directory
 
         #Move into the beamno directory
         beamno = os.listdir(observationpath)[0]
         beamnopath = os.path.join(observationpath, beamno)
+        #Move into the directory for the beam number
         os.chdir(beamnopath)
         print('In this directory', os.getcwd())
 
         #Move into the frequency directory
         freq = os.listdir(beamnopath)[0]
         freqpath = os.path.join(beamnopath, freq)
+        #Move into the directory for the frequency
         os.chdir(freqpath)
         print('In this directory', os.getcwd())
 
         #Set up file count system
         filecount = len(glob.glob1(freqpath, "*.ar"))
-        print(f'there are {filecount} files in observation {obs} for pulsar {pulsar}')
+        print('there are %s files in observation %s for pulsar %s' % (filecount, obs, pulsar))
 
         #Okay, now create the symbolic links
         print('creating soft links and directory')
 
         #Make a directory in MATTIME based on the information in the MEERTIME directory traced above
         mkdirpath2 = os.path.join(mkdirpath1, obs, beamno, freq)
-        os.makedirs(mkdirpath2)
-        
+        try:
+            os.makedirs(mkdirpath2)
+        except OSError as exc:
+            if exc.errno != errno.EEXIST:
+                raise
+            pass
         #Make an if path to detect if there is already a .linked file present
-        if glob.glob('*.linked'):
-            print('already copied/linked %s archives for %s', filecount, obs)
+        if glob.glob('done.txt'):
+            print('already copied/linked %s archives for %s' %(filecount, obs))
         else:
             #Creating soft links to the MEERTIME directory from MATTIME
             for archive in os.listdir(freqpath):
                 os.symlink(os.path.join(freqpath, archive), os.path.join(mkdirpath2, archive))
-            checkfile = os.path.join(mkdirpath2, ".linked")
+            checkfile = mkdirpath2 + "/" + "done.txt"
+            with open(checkfile,"w") as x:
+                x.write("this process has already been done")
+            #checkfile.write("This process has already been done")
+            #checkfile.close()
