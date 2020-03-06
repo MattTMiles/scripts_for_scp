e9ae05acdc7d8391674150200c040c5ccc69f0f0
MattTMiles <mtmil3@student.monash.edu>
2020-03-06 11:46:51 +1100

    code prior to pulsar path reroute

--- update.py
+++ update.py
@@ -2,10 +2,10 @@ import os
 import numpy as np
 #import pandas as pd
 import glob
-from pathlib import Path
+#from pathlib import Path
 import errno
-from astropy.table import Table
-from astropy.io import ascii
+#from astropy.table import Table
+#from astropy.io import ascii
 
 print('this is the update script')
 
@@ -15,9 +15,11 @@ os.chdir(chdirpath1)
 
 cwd = os.getcwd()
 print('searching for targets.dat in:',cwd)
-targets = open("targets.dat", newline="\n")
+targets = open("targets.dat")
 
 for pulsar in targets:
+    pulsar = pulsar.strip(' \n')
+    print({pulsar})
     #Make a directory for the data under MATTIME
     mkdirpath1 = os.path.join(chdirpath1, pulsar)
     try:
@@ -33,48 +35,56 @@ for pulsar in targets:
     os.chdir(timing)
     print('In this directory',os.getcwd())
     #In MEERTIME timing directory
+    for pulsar in os.listdir(timing):
+        pulsarpath = os.path.join(timing, pulsar)
+
+        #Move into the pulsar directory with command below
+        os.chdir(pulsarpath)
+        #In individual pulsar directory
+        print('In this directory', os.getcwd())
+        #observationpath = os.chdir(chdirpath3)
+
+        ##input here a way to set the beamno? Required?##
+
+        #Create a for loop for the observations and roll through these
         for obs in os.listdir(pulsarpath):
-            observationpath = os.path.join(pulsarpath,obs)
-            #Move into the direcotry for the individual observation
-            os.chdir(observationpath)
-            print('In this directory', os.getcwd())
-            #In individual pulsar directory
+        observationpath = os.path.join(pulsarpath,obs)
+        #Move into the direcotry for the individual observation
+        os.chdir(observationpath)
+        print('In this directory', os.getcwd())
+        #In individual pulsar directory
 
-            #The curent bug below is that the beamno comes out as a list and it can't be put into the
-            # the path line even when it I splat it, so I'll have to try a for loop fix which is a
-            # really bad way to do it probably.
+        #Move into the beamno directory
+        beamno = os.listdir(observationpath)[0]
+        beamnopath = os.path.join(observationpath,     beamno )
+        #Move into the directory for the beam number
+        os.chdir(beamnopath)
+        print('In this directory', os.getcwd())
 
-            #Move into the beamno directory
-            beamno = os.listdir(observationpath)[1:-1]
-            beamnopath = os.path.join(observationpath, beamno)
-            #Move into the directory for the beam number
-            os.chdir(beamnopath)
-            print('In this directory', os.getcwd())
+        #Move into the frequency directory
+        freq = os.listdir(beamnopath)[0]
+        freqpath = os.path.join(beamnopath, freq)
+        #Move into the directory for the frequency
+        os.chdir(freqpath)
+        print('In this directory', os.getcwd())
 
-            #Move into the frequency directory
-            freq = os.listdir(beamnopath)
-            freqpath = os.path.join(beamnopath, str(freq))
-            #Move into the directory for the frequency
-            os.chdir(freqpath)
-            print('In this directory', os.getcwd())
+        #Set up file count system
+        filecount = len(glob.glob1(freqpath, "*.ar"))
+        print(f'there are {filecount} files in observation {obs} for pulsar {pulsar}')
 
-            #Set up file count system
-            filecount = len(glob.glob1(freqpath, "*.ar"))
-            print('there are %s files in observation %s for pulsar %s', filecount, obs, pulsar)
+        #Okay, now create the symbolic links
+        print('creating soft links and directory')
 
-            #Okay, now create the symbolic links
-            print('creating soft links and directory')
+        #Make a directory in MATTIME based on the information in the MEERTIME directory traced above
+        mkdirpath2 = os.path.join(mkdirpath1, obs, beamno, freq)
+        os.makedirs(mkdirpath2)
 
-            #Make a directory in MATTIME based on the information in the MEERTIME directory traced above
-            mkdirpath2 = os.path.join(mkdirpath1, obs, beamno, freq)
-            os.mkdir(mkdirpath2)
-
-            #Make an if path to detect if there is already a .linked file present
-            if glob.glob('*.linked'):
-                print('already copied/linked %s archives for %s', filecount, obs)
-            else:
-                #Crating soft links to the MEERTIME directory from MATTIME
-                for archive in os.listdir(freqpath):
-                    os.symlink(os.path.join(freqpath, archive), mkdirpath2)
-                #Adding a .linked file to identify if this process needs to be done
-                Path('mkdirpath2/.linked').touch()
+        #Make an if path to detect if there is already a .linked file present
+        if glob.glob('*.linked'):
+            print('already copied/linked %s archives for %s', filecount, obs)
+        else:
+            #Crating soft links to the MEERTIME directory from MATTIME
+            for archive in os.listdir(freqpath):
+                os.symlink(os.path.join(freqpath, archive), mkdirpath2)
+            #Adding a .linked file to identify if this process needs to be done
+            Path('mkdirpath2/.linked').touch()
